c09025fe1862a47405fc44e0001bc2bc7b267f57
varadevishal07@gmail.com <varadevishal07@gmail.com>
2024-08-28 22:05:44 +0530

    all success

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const errorMiddleware = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
@@ -11,6 +12,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({path:"Config/config.env"});
 }
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "";
+export const BASE_URL = "https://mernecommerce-s7t5.onrender.com";
